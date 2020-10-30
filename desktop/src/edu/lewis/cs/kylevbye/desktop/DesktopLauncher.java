76a451967c6340fd913e81f1f7fbb39695544603
Kyle Bye <kylebye4mav@gmail.com>
2020-10-30 09:32:24 -0500

    Initial Commit

---
+++ desktop/src/edu/lewis/cs/kylevbye/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.lewis.cs.kylevbye.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.lewis.cs.kylevbye.DEHCECC;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DEHCECC(), config);
+	}
+}
